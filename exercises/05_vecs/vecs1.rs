6eb4faaaaee9b4c9fce9209ca411ee008d15dfcb
yan <yanyam.xue@gmail.com>
2025-10-05 19:45:00 -0700

    vectors

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -5,7 +5,10 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // Use the vector macro.
     // let v = ???;
 
+    let v = vec![10, 20 ,30 ,40];
+
     (a, v)
+
 }
 
 fn main() {
--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,11 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+
+        let num = element * 2; 
+
+        output.push(num);
+
     }
 
     output
@@ -24,7 +29,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+           element * 2 
         })
         .collect()
 }
